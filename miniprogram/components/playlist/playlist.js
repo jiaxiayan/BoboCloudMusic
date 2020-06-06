7761f4b0639a53b30dd197edb7ad276dc65f1bbf
pzh <1300209970@qq.com>
2020-06-06 11:37:25 +0800

    第一次提交

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,28 @@
+// components/playlis/playlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist: {
+      type:Object
+    }
+  },
+  observers:{
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
