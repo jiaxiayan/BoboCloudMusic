196e816df69fda80645517d2898c26ca501c4224
jiaxiayan <48024846+jiaxiayan@users.noreply.github.com>
2020-06-08 20:00:55 +0800

    Add files via upload
    上传

--- miniprogram/pages/playlist/playlist.js
+++ miniprogram/pages/playlist/playlist.js
@@ -1,105 +1,115 @@
 // pages/playlist/playlist.js
 const MAX_LIMIT = 15
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    "swiperImgUrl":[
-      {
-        url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
-  },
-  {
-        url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
-  },
-  {
-        url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
-  }
-    ],
-    playlist:[]
+    swiperImgUrls: [
+    //   {
+    //   url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+    // },
+    //   {
+    //     url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
+    //   },
+    //   {
+    //     url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
+    //   }
+      ],
+    playlist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this._getPlaylist()
+    this._getSwiper()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-      this.setData({
-        playlist:[]
-      })
-      this._getPlaylist()
-      const a = typeof(this)
-      console.log(this);
-      
+  onPullDownRefresh: function() {
+    this.setData({
+      playlist: []
+    })
+    this._getPlaylist()
+    this._getSwiper()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
     this._getPlaylist()
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  _getPlaylist(){
-  
+  _getPlaylist() {
     wx.showLoading({
       title: '加载中',
     })
     wx.cloud.callFunction({
-      name:'music',
-      data:{
-        start:this.data.playlist.length,
-        count:MAX_LIMIT,
-        $url:'playlist'
+      name: 'music',
+      data: {
+        start: this.data.playlist.length,
+        count: MAX_LIMIT,
+        $url: 'playlist',
       }
-    }).then((res) =>{
-      console.log(res);
+    }).then((res) => {
+      console.log(res)
       this.setData({
-        playlist:this.data.playlist.concat(res.result.data)
+        playlist: this.data.playlist.concat(res.result.data)
       })
-      wx.hideLoading()
       wx.stopPullDownRefresh()
+      wx.hideLoading()
     })
-}
+  },
+
+  _getSwiper(){
+    db.collection('swiper').get().then((res)=>{
+      this.setData({
+        swiperImgUrls: res.data
+      })
+      console.log(res);
+      
+    })
+  },
 })
